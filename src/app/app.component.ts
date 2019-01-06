22073d187f4cd91d33a7790ab9718707a99c51df
Raúl Piracés Alastuey <piraces@tanque.local>
2019-01-06 17:18:14 +0100

    First version of website

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,64 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { DOCUMENT } from '@angular/common';
+import { IPLookup } from '../interfaces/IPLookup';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
-  title = 'developerli';
+  domainAvailable = false;
+  domainAvailableChecked = undefined;
+  subdomain: any;
+  baseDomain = '.developer.li';
+  basePrefix = 'https://json.geoiplookup.io/';
+  kofiURL = 'https://ko-fi.com/F1F8OBQ5';
+  notOwned = 'DosArrest';
+
+  constructor(private http: HttpClient, @Inject(DOCUMENT) private document: any) {
+  }
+
+  public changeInput() {
+    this.domainAvailable = false;
+    this.domainAvailableChecked = undefined;
+  }
+
+  public supportMe() {
+    this.document.location.href = this.kofiURL;
+  }
+
+  public getActualClass() {
+    if (typeof this.domainAvailableChecked === 'undefined') {
+      return 'nes-input';
+    } else if (this.domainAvailableChecked) {
+      return ['nes-input', 'is-success'];
+    } else if (!this.domainAvailableChecked) {
+      return ['nes-input', 'is-error'];
+    }
+  }
+
+  public checkAvailability() {
+    if (this.subdomain && this.subdomain.length > 0) {
+      this.http
+        .get(this.basePrefix + this.subdomain + this.baseDomain)
+        .subscribe(
+          (data) => {
+            let response = data as IPLookup;
+            if (response && !response.org.includes(this.notOwned)) {
+              this.domainAvailable = false;
+              this.domainAvailableChecked = false;
+            } else {
+              this.domainAvailable = true;
+              this.domainAvailableChecked = true;
+            }
+          },
+          (err) => {
+            this.domainAvailable = false;
+            this.domainAvailableChecked = false;
+          });
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IPLookup } from '../interfaces/IPLookup';
 
 @NgModule({
   declarations: [
@@ -10,7 +13,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
